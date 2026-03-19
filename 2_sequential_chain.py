3584384d18f3ded3a153336d65ef9398dc6e3354
Pranshu Sama <23je0733@iitism.ac.in>
2026-03-19 15:52:19 +0530

    used different project name inside code and created tags for metadata

--- 2_sequential_chain.py
+++ 2_sequential_chain.py
@@ -2,6 +2,9 @@ from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+import os
+
+os.environ['LANGCHAIN_PROJECT'] = 'Sequential LLM App'
 
 load_dotenv()
 
@@ -15,12 +18,20 @@ prompt2 = PromptTemplate(
     input_variables=['text']
 )
 
-model = ChatGroq(model="llama-3.3-70b-versatile")
+model1= ChatGroq(model="llama-3.3-70b-versatile",temperature=0.7)
+model2 = ChatGroq(model="llama-3.1-8b-instant",temperature=0.5) 
 
 parser = StrOutputParser()
 
-chain = prompt1 | model | parser | prompt2 | model | parser
+chain = prompt1 | model1 | parser | prompt2 | model2 | parser
 
-result = chain.invoke({'topic': 'Unemployment in India'})
+config = {
+    'run_name' : 'sequential chain',
+    'tags' : ['llm-app','report-generation','summarization'],
+    'metadata' : {'model1' : 'llama-3.3-70b-versatile', 'model1_temp' : 0.7 , 'parser' : 'stroutputparser',
+                  'model2' : 'llama-3.1-8b-instant', 'model2_temp' : 0.5, 'parser' : 'stroutputparser'}
+}
+
+result = chain.invoke({'topic': 'Unemployment in India'},config=config)
 
 print(result)
